713ee74f509d4f7a4c265af34b0e58dcf69bb668
Kayne <kayne@ciand.com>
2021-01-13 15:55:00 -0300

    Implementation of color variables

---
+++ src/styles/colors.ts
@@ -0,0 +1,39 @@
+export default {
+  background_color_01: '#FCFCFC',
+  background_color_02: '#EFEFEF',
+  background_color_03: '#DFDFDF',
+
+  primary_color_01: '#003a78',
+  primary_color_02: '#2B5B8E',
+  primary_color_03: '#5A6B75',
+  primary_color_04: '#00C8B0',
+
+  light_color_01: '#FCF7DE',
+
+  positive_color_01: '#DEFCE9',
+  positive_color_02: '#99D6AF',
+  positive_color_03: '#60BF83',
+  positive_color_04: '#3A925A',
+
+  neutral_color_01: '#FFFFFF',
+  neutral_color_02: '#F7F7F7',
+  neutral_color_03: '#EDEDED',
+  neutral_color_04: '#BEBEBE',
+  neutral_color_05: '#6C6E7B',
+  neutral_color_06: '#131313',
+
+  alert_color_01: '#FCE2DE',
+  alert_color_02: '#F0B2AA',
+  alert_color_03: '#F08678',
+  alert_color_04: '#F03319',
+
+  warning_color_01: '#FEF3BE',
+  warning_color_02: '#FDE05E',
+  warning_color_03: '#F9CB04',
+  warning_color_04: '#B59403',
+
+  shadow_color_01: '#1313131A',
+  shadow_color_02: '#0000001A',
+
+  transparent: 'rgb(0, 0, 0, 0)',
+};
